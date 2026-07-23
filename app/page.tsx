86a7af477bafede2913bdcbeaa9442873fb1d7e8
k0turbo <robert0xtwap@gmail.com>
2026-07-23 16:52:28 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -259,7 +259,7 @@ export default function LaunchingSoon() {
 
         {/* Featured Card */}
         <Link 
-          href="/blog/rethinking-the-gold-thesis"
+          href="/blog/xaus-is-live"
           className="group block w-full max-w-2xl border border-[#222222] rounded-xl overflow-hidden bg-[#0A0A0A] hover:border-[#444444] transition-colors duration-300">
         
           {/* Card Top: Image */}
