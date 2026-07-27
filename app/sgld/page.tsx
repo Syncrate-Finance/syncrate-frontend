83d549efa5bb4e22d7b787978b766bfbd1558f0b
k0 <300018102+k0turbo@users.noreply.github.com>
2026-07-27 16:20:24 +0100

    Update page.tsx

--- app/sgld/page.tsx
+++ app/sgld/page.tsx
@@ -15,7 +15,7 @@ import { parseUnits, formatUnits } from 'viem'
 const IS_LIVE = false;
 
 const XAUS_ADDRESS = '0x10C5E0643bCc6C915Cad0335f70A96c1532766eb' as const
-const SGLD_VAULT_ADDRESS = '0x2123541BE6F7dA2b429BC703A591944DC7Db26a3' as const
+const SGLD_VAULT_ADDRESS = '0x9dc4a31FAF646DB5051b9e14520900387E3e3B7a' as const
 
 const VAULT_INCEPTION = new Date('2026-06-01T00:00:00Z').getTime()
 
