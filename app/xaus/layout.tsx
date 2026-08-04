45d72e15fd4b5bd55b198a1b071ee16dfeec332e
k0 <300018102+k0turbo@users.noreply.github.com>
2026-08-04 14:00:59 +0100

    Update layout.tsx

--- app/xaus/layout.tsx
+++ app/xaus/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     url: 'https://syncrate.org/xaus',
     images: [
       {
-        url: '/embed.PNG?v=2',
+        url: '/embed.PNG',
         width: 1200,
         height: 600,
         alt: 'Syncrate Gold',
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'Syncrate',
-    images: ['/embed.PNG?v=2'],
+    images: ['/embed.PNG'],
   },
 }
 
