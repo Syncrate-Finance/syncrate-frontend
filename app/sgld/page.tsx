be5227a1c1936f8ab517cdf37ea9aa35267e8d6f
k0 <kizito@syncrate.org>
2026-08-08 12:32:50 +0100

    Update page.tsx

--- app/sgld/page.tsx
+++ app/sgld/page.tsx
@@ -12,12 +12,12 @@ import { parseUnits, formatUnits } from 'viem'
 // ==========================================
 // 🚀 CONFIGURATIONS
 // ==========================================
-const IS_LIVE = false;
+const IS_LIVE = true;
 
 const XAUS_ADDRESS = '0x10C5E0643bCc6C915Cad0335f70A96c1532766eb' as const
 const SGLD_VAULT_ADDRESS = '0xbA93Aa354a3B47A936d713b8FB90126D4d35546B' as const
 
-const VAULT_INCEPTION = new Date('2026-06-01T00:00:00Z').getTime()
+const VAULT_INCEPTION = new Date('2026-08-08T00:00:00Z').getTime()
 
 const erc20Abi = [
   { type: 'function', name: 'approve', inputs: [{ name: 'spender', type: 'address' }, { name: 'amount', type: 'uint256' }], outputs: [{ type: 'bool' }], stateMutability: 'nonpayable' },
